088b0cd339efbce5cc57ec1f987dc0ce4ef6ff7c
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-04-14 17:04:05 +0200

    .

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,13 @@
 const {alias, configPaths} = require('react-app-rewire-alias')
 
 module.exports = function override(config) {
-  return alias(configPaths('./tsconfig.paths.json'))(config)
+  const modifiedConfig = {
+    ...config,
+    resolve: {
+      ...config.extensions,
+      extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+  }
+
+  return alias(configPaths('./tsconfig.paths.json'))(modifiedConfig)
 }
