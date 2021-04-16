256bec3e32cf3bb1ee7d6580dd3e3afddb2a5130
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-04-16 20:10:53 +0200

    remove type

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,5 +6,3 @@ type Theme = typeof theme;
 declare module 'styled-components' {
   export interface DefaultTheme extends Theme {}
 }
-
-export type AppAction<K, V = void> = V extends void ? { type: K } : { type: K } & V;
